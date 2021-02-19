9617d33212f9775047b5da154a5ce9a3425b9271
RKHTechnologies <rhysh6@gmail.com>
2021-02-19 23:03:36 +0000

    Added in many shared files and additional libraries. Included new logos/ graphics for site

---
+++ src/Shared/ImageLib.tsx
@@ -0,0 +1,88 @@
+import rkh from '../img/RKHT_logo.png';
+import bear from '../img/bear.png'
+import groupImage from '../img/desktop/group-of-scouts-in-helmets.jpg';
+import groupImageMobile from '../img/mobile/group-of-scouts-in-helmets.jpg';
+
+// Section images
+import beavers360 from '../img/beavers-singing.jpg';
+import cubs360 from '../img/cubs-marshmallow.jpg';
+import scouts360 from '../img/scouts-in-a-row.jpg';
+import explorers360 from '../img/explorers-on-rock.jpg';
+
+// Hero Images
+import highRopesHero from '../img/desktop/cub-high-ropes.jpg';
+import kayakingHero from '../img/desktop/kayaking.jpg';
+import expeditionHero from '../img/desktop/explorer-expedition.jpg';
+import beaversHero from '../img/beavers-and-leader.jpg';
+import cubsHero from '../img/cubs-group.jpg';
+import scoutsHero from '../img/scouts-life-jackets.jpg';
+  // Mobile
+import highRopesHeroMobile from '../img/mobile/cub-high-ropes.jpg';
+import kayakingHeroMobile from '../img/mobile/kayaking.jpg';
+import expeditionHeroMobile from '../img/mobile/explorer-expedition.jpg';
+import beaversHeroMobile from '../img/mobile/beavers-and-leader.jpg';
+import cubsHeroMobile from '../img/mobile/cubs-group.jpg';
+import scoutsHeroMobile from '../img/mobile/scouts-life-jackets.jpg';
+
+// Beavers Characters
+import characters from '../img/desktop/beavers-characters.png';
+
+// Fluer De Lis
+import FDL_Blue from '../img/FDL_Blue.png';
+import FDL_Green from '../img/FDL_Green.png';
+import FDL_Purple from '../img/FDL_Purple.png';
+import FDL_Red from '../img/FDL_Red.png';
+
+
+
+export const ImagesDesktop = {
+  'rkh': rkh,
+  'bear': bear,
+  'groupImage': groupImage,
+  'beaversSection': beavers360,
+  'cubsSection': cubs360,
+  'scoutsSection': scouts360,
+  'explorersSection': explorers360,
+  
+  'highRopesHero': highRopesHero,
+  'kayakingHero': kayakingHero,
+  'expeditionHero': expeditionHero,
+  'beaversHero': beaversHero,
+  'cubsHero': cubsHero,
+  'scoutsHero': scoutsHero,
+
+  'characters': characters,
+  
+  'FDL_Blue': FDL_Blue,
+  'FDL_Green': FDL_Green,
+  'FDL_Purple': FDL_Purple,
+  'FDL_Red': FDL_Red,
+}
+
+
+
+export const ImagesMobile = {
+  'rkh': rkh,
+  'bear': bear,
+  'groupImage': groupImageMobile,
+  'beaversSection': beavers360,
+  'cubsSection': cubs360,
+  'scoutsSection': scouts360,
+  'explorersSection': explorers360,
+  
+  'highRopesHero': highRopesHeroMobile,
+  'kayakingHero': kayakingHeroMobile,
+  'expeditionHero': expeditionHeroMobile,
+  'beaversHero': beaversHeroMobile,
+  'cubsHero': cubsHeroMobile,
+  'scoutsHero': scoutsHeroMobile,
+
+  'characters': characters,
+  
+  'FDL_Blue': FDL_Blue,
+  'FDL_Green': FDL_Green,
+  'FDL_Purple': FDL_Purple,
+  'FDL_Red': FDL_Red,
+}
+
+export type imageLib = 'rkh' | 'bear' | 'groupImage' | 'beaversSection' | 'cubsSection' | 'scoutsSection' | 'explorersSection' | 'highRopesHero' | 'kayakingHero' | 'expeditionHero' | 'beaversHero' | 'cubsHero' | 'scoutsHero' | 'characters' | 'FDL_Blue' | 'FDL_Green' | 'FDL_Purple' | 'FDL_Red';
