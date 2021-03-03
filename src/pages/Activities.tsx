06f53d6bdfa287f28db3f99072d70690574820db
RKHTechnologies <rhysh6@gmail.com>
2021-03-03 22:22:28 +0000

    Activities header

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -1,10 +1,37 @@
 import React, { FC } from 'react';
-import { PageBodyContainer } from '../Shared/SharedStyles';
+import styled from 'styled-components';
+import Hero from '../Shared/Hero';
+import { colours, PageBodyContainer, SharedSettings } from '../Shared/SharedStyles';
+
+const TextOverlay = styled.div`
+  font-size: 6vw;
+  font-weight: 400;
+  width: 100%;
+  height: 65%;
+  color: ${colours.light};
+  position: absolute;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(115,115,115,0.15);
+
+  @media(max-width: ${SharedSettings.mobile}) {
+    font-size: 10vw;
+    font-weight: 600;
+  }
+
+  @media(max-width: 700px) {
+    height: 300px;
+  }  
+`;
 
 const Activities: FC = () => {
   return (
     <PageBodyContainer>
-      Activities
+      <Hero image="highRopesHero" small/>
+      <TextOverlay>Activities</TextOverlay>
+
     </PageBodyContainer>
   );
 }
