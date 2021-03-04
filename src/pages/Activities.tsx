c9e319355a9ad0c480904d48142cd48f98c35faf
RKHTechnologies <rhysh6@gmail.com>
2021-03-04 08:08:25 +0000

    Activities page layout

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -27,15 +27,49 @@ const TextOverlay = styled.div`
   }  
 `;
 
-const Activity = styled.div`
+const ActivitiesContainer = styled.div`
   width: 100%;
-  background: #b1b1b1;
-  height: 350px;
-  margin-top: 80px;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+`;
+
+const Activity = styled.div`
+  width: 400px;
+  background: #F6F6F6;
+  height: 380px;
+  margin-top: 50px;
+  box-shadow: 1px 2px 3px 1px #19191914;
+  padding: 20px;
+  box-sizing: border-box;
+  transition: all 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
 const Image = styled.div`
   width: 100%;
+  background: #e0e0e0;
+  height: 250px;
+`;
+
+const Header = styled.div`
+  font-size: 2.2em;
+  font-weight: 600;
+  text-align: center;
+  margin: 5px 0;
+`;
+
+const Button = styled.div`
+  background: ${colours.Teal};
+  color: ${colours.light};
+  font-weight: 400;
+  text-align: center;
+  padding: 10px 0;
+  border-radius: 4px;
 `;
 
 const Activities: FC = () => {
@@ -44,13 +78,62 @@ const Activities: FC = () => {
       <Hero image="highRopesHero" small/>
       <TextOverlay>Activities</TextOverlay>
 
-      <Section id="Bookings">
+      <Section id="Activities">
         <div className="wrapper">
-          <Activity>
-            <Image />
-          </Activity>      
+          <ActivitiesContainer>
+            
+            <Activity>
+              <Image />
+              <Header>Low Rope Area</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Archery</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Climbing</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Air Rifles</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Pioneering</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Low Rope Area</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Boulders</Header>
+              <Button>Find out more</Button>
+            </Activity>
+
+            <Activity>
+              <Image />
+              <Header>Frisbee Golf</Header>
+              <Button>Find out more</Button>
+            </Activity>
+          </ActivitiesContainer>
         </div>
       </Section>
+
+    
 
     </PageBodyContainer>
   );
