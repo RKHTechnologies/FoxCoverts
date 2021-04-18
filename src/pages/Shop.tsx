a1d8751855378929aa32fe065947808462308d90
RKHTechnologies <rhysh6@gmail.com>
2021-04-18 22:41:04 +0100

    Started shop

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from 'react';
-import { PageBodyContainer } from '../Shared/SharedStyles';
+import styled from 'styled-components';
+import { colours, PageBodyContainer } from '../Shared/SharedStyles';
+
+const ShopContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f1f1f1;
+`;
+
+const ShopItem = styled.div`
+  width: 200px;
+  height: 240px;
+  border-radius: 8px;
+  background: ${colours.light};
+  box-shadow: 3px 5px 9px #c3c3c3;
+`;
+
+
 
 const Shop: FC = () => {
   return (
     <PageBodyContainer>
-      Shop
+      <ShopContainer>
+        <ShopItem>
+
+        </ShopItem>
+      </ShopContainer>
     </PageBodyContainer>
   );
 }
