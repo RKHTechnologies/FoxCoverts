39564b5f793e0a6b4ec71a52ac39546b4452d0df
RKH <rhysh6@gmail.com>
2021-04-30 16:02:13 +0100

    Working on read

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,8 +1,16 @@
-import React, { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styled from 'styled-components';
 import ShopItem from '../Components/ShopItem';
-import { colours, PageBodyContainer } from '../Shared/SharedStyles';
+import { PageBodyContainer } from '../Shared/SharedStyles';
 import firebase from '../util/firebase';
+
+interface IPageItem {
+  name: string;
+  price: string;
+  image: string;
+}
+
+let pageItems: Array<IPageItem> = [];
 
 const ShopContainer = styled.div`
   width: 100%;
@@ -13,15 +21,12 @@ const ShopContainer = styled.div`
   background: #f1f1f1;
 `;
 
-
 const Shop: FC = () => {
   const handleClick = () => {
     createShopItem();
   };
 
-  const createShopItem = () => {
-    const itemName = "Beavers Jumper Small";
-  
+  const createShopItem = () => { 
     const ref = firebase.database().ref("newItem");
     const newItem = {
       name: "Beaver new thing",
@@ -32,16 +37,45 @@ const Shop: FC = () => {
     ref.push(newItem);
   };
 
+  // Read
+  useEffect(() => {
+    const shopItems = firebase.database().ref("newItem");
+    shopItems.on("value", (snapshot: any) => {
+      console.log(snapshot.val());
+      
+      for (const item in snapshot.val()) {
+        debugger;
+        console.log(item);
+        // pageItems.push(item);
+      }
+    });
+  }, []);
+
+  const shopPageItems = () => {
+    debugger;
+    console.log("page items:", pageItems);
+
+    // pageItems.forEach(item => console.log(item));
+
+    for (const testprop in pageItems) {
+      console.log("testprop:", testprop);
+    }
 
 
+  }
 
   return (
     <PageBodyContainer>
       <ShopContainer>
-        <ShopItem name="Beaver Scouts Reflective Soft Shell Jacket - Kids" price="28" image="test" />
+        {pageItems.forEach(item => console.log("test"))}
+
+             {/* // <ShopItem name={item.name} price={item.price} image={item.image} />
+          
+          // console.log(`Name: ${item.name}, Price: ${item.price}, Image: ${item.image}`) */}
+        {/* <ShopItem name="Beaver Scouts Reflective Soft Shell Jacket - Kids" price="28" image="test" />
         <ShopItem name="Stay Wild double wall insulated water bottle 500ml" price="15" image="test" />
-        <ShopItem name="Garden Games - Limbo" price="25" image="test" />
-        <div onClick={handleClick}>test</div>
+        <ShopItem name="Garden Games - Limbo" price="25" image="test" /> */}
+        <div onClick={shopPageItems}>test</div>
       </ShopContainer>
     </PageBodyContainer>
   );
