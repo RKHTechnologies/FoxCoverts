e58ebd4e0852882605f406afbbe0f84a11955c0b
RKHTechnologies <rhysh6@gmail.com>
2021-05-20 22:45:39 +0100

    Accommodation save

--- src/pages/Accommodation.tsx
+++ src/pages/Accommodation.tsx
@@ -3,7 +3,16 @@ import styled from 'styled-components';
 import { PageBodyContainer } from '../Shared/SharedStyles';
 
 const AccommodationContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 40px;
+`;
 
+const AccommodationItem = styled.div`
+  width: 350px;
+  height: 700px;
+  background: #acacac;
 `;
 
 const Accommodation: FC = () => {
@@ -11,7 +20,11 @@ const Accommodation: FC = () => {
     <PageBodyContainer>
       <AccommodationContainer>
         
-
+        <AccommodationItem></AccommodationItem>
+        <AccommodationItem></AccommodationItem>
+        <AccommodationItem></AccommodationItem>
+        <AccommodationItem></AccommodationItem>
+        <AccommodationItem></AccommodationItem>
 
       </AccommodationContainer>
     </PageBodyContainer>
